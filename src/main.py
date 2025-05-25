47e7e4b049bae98857be5f72509b883dbf64026a
Manan Jain <174644789+MananJain39@users.noreply.github.com>
2025-05-25 13:59:00 +0530

    feat: add white pegs logic

--- src/main.py
+++ src/main.py
@@ -28,6 +28,7 @@ def detection():
                 continue
 
         red_pegs = 0
+        white_pegs = 0
 
         guess_rem = (
             []
@@ -48,7 +49,35 @@ def detection():
                 )  # this adds the unused element to the empty list we created
                 secret_code_rem.append(secret_code[i])  # this is same as above
 
-        print(f"{red_pegs} R")
+        # g_rem_copy = guess_rem #this does not copy the list
+        # instead of this in python there is a inbuilt function named .copy()
+        # or use g_rem_copy = list(guess_rem)
+        # or use the slicing method which is the fasted and the most useful that is g_rem_copy = guess_rem[:]
+
+        g_rem_copy = list(guess_rem)
+        secret_code_rem_copy = list(secret_code_rem)
+
+        # for white pegs
+        # for white pegs we will only use the used list as there can be a crossover of elements
+
+        # for i in range(
+        #     len(g_rem_copy)  # this does not work as you are modifying the same list
+        # ):
+        #     for j in range(len(secret_code_rem_copy)):
+        #         if guess_rem[i] == secret_code_rem[j]:
+        #             white_pegs += 1
+        #             guess_rem.remove(guess_rem[i])
+        #             secret_code_rem.remove(secret_code_rem[j])
+
+        for i in range(len(g_rem_copy)):
+            for j in range(len(secret_code_rem_copy)):
+                if g_rem_copy[i] == secret_code_rem_copy[j]:
+                    white_pegs += 1
+                    g_rem_copy[i] = None
+                    secret_code_rem_copy[j] = None
+                    break
+
+        print(f"{red_pegs} R , {white_pegs} W")
 
         if red_pegs == 4:
             print("You Guessed the code.")
