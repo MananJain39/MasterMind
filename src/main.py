2b82e094f90bb2e36ed99a104ef6fb8d5452cfd7
Manan Jain <174644789+MananJain39@users.noreply.github.com>
2025-05-26 16:19:28 +0530

    feat: add menu and difficulty range

--- src/main.py
+++ src/main.py
@@ -1,18 +1,34 @@
 import random
 
 
-def generation():
-    numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "9"]
-    secret_code = random.sample(numbers, 4)
-    return [int(x) for x in secret_code]
+def generation(diff_range):
+    # numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    # secret_code = random.choices(numbers, k=4)
+    # # return [int(x) for x in secret_code]
+
+    if diff_range == 5:
+        numbers = ["0", "1", "2", "3", "4", "5"]
+        secret_code = random.choices(numbers, k=4)
+        return [int(x) for x in secret_code]
+    if diff_range == 7:
+        numbers = ["0", "1", "2", "3", "4", "5", "6", "7"]
+        secret_code = random.choices(numbers, k=4)
+        return [int(x) for x in secret_code]
+    if diff_range == 9:
+        numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        secret_code = random.choices(numbers, k=4)
+        return [int(x) for x in secret_code]
 
 
-def detection():
-    secret_code = generation()
+def detection(secret_code):
+    print(f"{secret_code}")
     attempts = 10
     for num in range(1, 11):
         # for invalid attempts
         while True:
+            # this is like a trial and error block which basically tells like try all the method and if hits the error go to except
+            # it also has stuff like else and finally
+            # except is just a nice way to show the error instead of those 100 lines of random code so the user doesnt feel like something big happened
             try:
 
                 guess = [
@@ -50,9 +66,9 @@ def detection():
                 secret_code_rem.append(secret_code[i])  # this is same as above
 
         # g_rem_copy = guess_rem #this does not copy the list
-        # instead of this in python there is a inbuilt function named .copy()
+        # instead of this, in python there is a inbuilt function named .copy()
         # or use g_rem_copy = list(guess_rem)
-        # or use the slicing method which is the fasted and the most useful that is g_rem_copy = guess_rem[:]
+        # or use the SLICING METHOD which is the fasted and the most useful that is g_rem_copy = guess_rem[:]
 
         g_rem_copy = list(guess_rem)
         secret_code_rem_copy = list(secret_code_rem)
@@ -83,6 +99,35 @@ def detection():
             print("You Guessed the code.")
         elif num == attempts:
             print("Attempts over.")
+            print(f"Secret code was {secret_code}")
 
 
-detection()
+def menu():
+    print("Welcome to MasterMind.")
+    print("Choose your difficulty.")
+    print("1. Easy(0-5)")
+    print("2. Medium(0-7)")
+    print("3. Hard(0-9)")
+    diff_range = 0
+    while True:  # match case will only work if you have python 3.10+
+        # suggested to use if else for wider compatibility
+        choice = input("Enter your Choice: ")
+        match choice:
+            case "1":
+                diff_range = 5
+                break
+            case "2":
+                diff_range = 7
+                break
+            case "3":
+                diff_range = 9
+                break
+            case _:
+                print("Enter from 1, 2, 3 only")
+
+    secret_code = generation(diff_range)
+    detection(secret_code)
+
+
+if __name__ == "__main__":
+    menu()
